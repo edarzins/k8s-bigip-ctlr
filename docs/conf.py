d5ba69f34e99b3d1e17f6cfb2358339d44a538c8
Erik Darzins <e.darzins@.f5.com>
2017-11-28 20:20:21 -0700

    fix versions

--- docs/conf.py
+++ docs/conf.py
@@ -77,12 +77,12 @@ author = u'F5 Networks'
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
-f = open('../next-version.txt')
-v = f.readline().split('.')
-# The short X.Y version.
-version = u'v{}.{}'.format(v[0], v[1])
-# The full version, including alpha/beta/rc tags.
-release = u'v{}.{}.{}-dev'.format(v[0], v[1], v[2]) 
+with open('../next-version.txt') as verfile:
+    v = verfile.readline().strip().split('.')
+    # The short X.Y version.
+    version = u'v{}.{}'.format(v[0], v[1])
+    # The full version, including alpha/beta/rc tags.
+    release = u'v{}.{}.{}-dev'.format(v[0], v[1], v[2])
 
 # def setup(app):
 #    app.add_config_value('versionlevel', '', 'env')
